cc222d58e1181f330241ed89bc0ba1fe199e6e7d
Ahmet Taha Arslan <ahmet-taha-arslan@hotmail.com>
2022-10-04 15:25:10 +0300

    worked

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 const express=require('express')
+const ejs = require('ejs')
 
 
-
-const app = express()
-
+ const app = express()
 
 
-const port = 3000;
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs')
 
+
+//MIDDLEWARES
+app.use(express.static('public'))
+
+
+//ROUTES
 app.get('/', (req,res) => {
-    res.status(200).send('heyy')
+    res.status(200).render(
+        'index', {
+        page_name:'index'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.status(200).render(
+        'about', {
+        page_name:'about'
+        })
 })
 
 
+
+
+
+
+
+//SERVER
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`app started at ${port}`)
