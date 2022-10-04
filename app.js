a47edee357430f37d05928d474d03d141032ab9c
Ahmet Taha Arslan <ahmet-taha-arslan@hotmail.com>
2022-10-04 14:37:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require('express')
+
+
+
+const app = express()
+
+
+
+const port = 3000;
+
+app.get('/', (req,res) => {
+    res.status(200).send('heyy')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`app started at ${port}`)
+})
